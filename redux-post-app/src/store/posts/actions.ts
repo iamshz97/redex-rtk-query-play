df5302cce2fc939528c2aad5bc69453e9aa928ed
Shazni Shiraz <shaznishiraz@gmail.com>
2023-04-02 16:52:12 +0530

    REDUX - CREATE POST ACTION

---
+++ redux-post-app/src/store/posts/actions.ts
@@ -0,0 +1,12 @@
+import { PostActionTypes } from "./constants";
+import { Post, PostAction } from "./types";
+
+export const addPost = (value: Post): PostAction => ({
+  type: PostActionTypes.ADD_POST,
+  post: value,
+});
+
+export const removePost = (value: Post): PostAction => ({
+  type: PostActionTypes.REMOVE_POST,
+  post: value,
+});
