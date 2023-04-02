2418eccd79eafbbc86e708d1a5fda99e60e0344d
Shazni Shiraz <shaznishiraz@gmail.com>
2023-04-02 16:53:34 +0530

    Update App.tsx to utilize the redux store to manage posts

--- redux-post-app/src/App.tsx
+++ redux-post-app/src/App.tsx
@@ -1,35 +1,120 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addPost, removePost } from "./store/posts/actions";
+import { Post } from "./store/posts/types";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(): JSX.Element {
+  const [newPost, setNewPost] = useState<Post>({
+    id: 1,
+    title: "",
+    content: "",
+    author: "",
+    publishedAt: new Date(),
+    tags: [],
+  });
+
+  const dispatch = useDispatch();
+  const posts = useSelector((state: any) => state.post.posts);
+
+  const handleAddPost = (): void => {
+    dispatch(addPost(newPost));
+    setNewPost({
+      ...newPost,
+      id: newPost.id + 1,
+      title: "",
+      content: "",
+      author: "",
+      publishedAt: new Date(),
+      tags: [],
+    });
+  };
+
+  const handleRemovePost = (post: Post): void => {
+    dispatch(removePost(post));
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <header>
+        <h1>Redux Add/ Remove Posts</h1>
+      </header>
+      <main>
+        <section className="new-post-section">
+          <h2>Add a new post</h2>
+          <div className="card">
+            <form onSubmit={(e) => e.preventDefault()}>
+              <label htmlFor="title">Title</label>
+              <input
+                id="title"
+                type="text"
+                placeholder="Title"
+                value={newPost.title}
+                onChange={(e) =>
+                  setNewPost({ ...newPost, title: e.target.value })
+                }
+              />
+
+              <label htmlFor="content">Content</label>
+              <textarea
+                id="content"
+                placeholder="Content"
+                value={newPost.content}
+                onChange={(e) =>
+                  setNewPost({ ...newPost, content: e.target.value })
+                }
+              />
+
+              <label htmlFor="author">Author</label>
+              <input
+                id="author"
+                type="text"
+                placeholder="Author"
+                value={newPost.author}
+                onChange={(e) =>
+                  setNewPost({ ...newPost, author: e.target.value })
+                }
+              />
+
+              <button onClick={handleAddPost}>Add post</button>
+            </form>
+          </div>
+        </section>
+        <section className="all-posts-section">
+          <h2>All posts</h2>
+          <div className="card-list">
+            {posts.map((post: Post) => (
+              <div className="card" key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.content}</p>
+                <p>
+                  <span>By </span>
+                  {post.author}
+                </p>
+                <p>
+                  <span>Published on </span>
+                  {post.publishedAt.toLocaleDateString()}
+                </p>
+                <div>
+                  {post.tags.map((tag: string) => (
+                    <span className="tag" key={tag}>
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+                <button onClick={() => handleRemovePost(post)}>Remove</button>
+              </div>
+            ))}
+          </div>
+        </section>
+      </main>
+      <footer>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
